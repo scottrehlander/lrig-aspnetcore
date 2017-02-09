dec889063dc752739587295941acd8fc3df333bb
scoot <scott@rehlander.com>
2017-02-08 23:50:44 -0500

    - Clean up the nav and add a chapters nav link (TODO: Add chapters component)

--- LrigGlobal/ClientApp/app/components/navmenu/navmenu.component.ts
+++ LrigGlobal/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'nav-menu',
     template: require('./navmenu.component.html'),
-    styles: [require('./navmenu.component.css')]
+    styles: [require('./navmenu.component.css')],
+    providers: [AuthService]
 })
 export class NavMenuComponent {
+
+    constructor(private auth: AuthService) { }
+
+    logout(event) {
+        event.preventDefault();
+        this.auth.logout();
+    }
+
 }
